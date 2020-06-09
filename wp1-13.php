543d20cae3064694949b21438ef0bb9b30dcb73b
Libor-1 <60381836+Libor-1@users.noreply.github.com>
2020-06-09 09:20:59 +0200

    Add files via upload

---
+++ wp1-13.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="submit2.php" method="post">
+<label for="A">Číslo 1</label>
+<input type="number" name="A" id="A">
+<br>
+
+<label for="B">Číslo 2</label>
+<input type="number" name="B" id="B">
+<br>
+<input type="submit" value="Vypočítat">
+
+</form>
+
+</body>
+</html>
